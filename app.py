1b77554e426fe9feab1d868c8beea39af7d336b4
Olly Ewert <olly@fantail.co>
2022-05-10 12:19:02 +1200

    add dynamic element

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 # Special thanks to https://codefresh.io/docker-tutorial/hello-whale-getting-started-docker-flask/ for the code
 
-from flask import Flask
+from flask import Flask, render_template
+from random import Random
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_whale():
-    return "Whale, Hello there!"
+    value = Random().randint(0,1000)
+    demo_var = f"This is dynamic: {value}"
+    return render_template('index.html', demo_var = demo_var)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
