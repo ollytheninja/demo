5f17dc952f043004f942381d6504a4674c7c865d
Olly Ewert <olly@fantail.co>
2022-05-10 13:07:10 +1200

    show secrets from Vault

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 # Special thanks to https://codefresh.io/docker-tutorial/hello-whale-getting-started-docker-flask/ for the code
-
-from flask import Flask, render_template
+from pathlib import Path
 from random import Random
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_whale():
-    value = Random().randint(0,1000)
+    value = Random().randint(0, 1000)
     demo_var = f"This is dynamic: {value}"
-    return render_template('index.html', demo_var = demo_var)
+
+    secrets_dir = Path("/vault/secrets/")
+
+    secrets = {}
+    if secrets_dir.exists():
+        for file in secrets_dir.iterdir():
+            secrets[file.name] = file.open().read()
+
+    if not secrets_dir.exists():
+        secrets = None
+
+    return render_template('index.html', demo_var=demo_var, secrets_dir=secrets_dir, secrets=secrets)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
